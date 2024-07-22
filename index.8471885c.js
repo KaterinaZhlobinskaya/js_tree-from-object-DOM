621a3d8f1d697291fd9de907eb94c438b05f1b93
Katya <evil.666.buny@gmail.com>
2024-07-22 16:31:01 +0300

    make build

---
+++ index.8471885c.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in n.appendChild(r),t){var l=document.createElement("li");l.textContent=a,r.appendChild(l),Object.keys(t[a]).length>0&&e(l,t[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.8471885c.js.map
